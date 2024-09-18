c59591210e9b958aceb47f82f198f95a187efaf3
khulekani492 <77571282+khulekani492@users.noreply.github.com>
2024-09-18 23:28:08 +0200

    Update elevator.py

--- elevator.py
+++ elevator.py
@@ -55,3 +55,51 @@ for k,v in cards.items():
 user_name = input("Name: ")
 num = int(input("Enter floor number: ")) 
 _elavator(levels,num)
+
+
+
+import random
+rng = random.Random()
+# create a deck 
+Card = []
+numbers = [0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,]
+cards = {'Red ':numbers,'Yellow ':numbers,'blue ':numbers,'green ':numbers}
+
+special_cards = []
+for i in ['Red','RED','YELLOW','YELLOW','GREEN','GREEN','BLUE', 'BLUE']:
+      c = "+2" + i
+      special_cards.append(c)
+for i in ['Red','RED','YELLOW','YELLOW','GREEN','GREEN','BLUE', 'BLUE']:
+      b = "REV_" + i
+      special_cards.append(b)
+for i in ['Red','RED','YELLOW','YELLOW','GREEN','GREEN','BLUE', 'BLUE']:
+    b = "SKIP^" + i
+    special_cards.append(b)
+
+for i in range(1,5):
+      wild_card = 'wild' + str(i)
+      special_cards.append(wild_card)
+
+
+for i in range(1,5):
+      wild_card = 'wildraw' + str(i)
+      special_cards.append(wild_card)
+for k,v in cards.items():
+        for i in v:
+            a = [k , str(i)]
+            Card.append(a)
+Card.extend(special_cards)
+
+
+#shuffle_the_card 
+rng.shuffle(Card)
+#print((Card))
+# Games main logic 
+Computer_hand = Card[1:8]
+players_hand = Card[8:15] 
+
+
+print('computer hand ',Computer_hand)
+print('player_hand ',players_hand)
+
+Discarded_pile = []
