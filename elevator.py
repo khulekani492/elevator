cb66c89a348ac8935223c1b86c9b81e41150ef69
khulekani492 <77571282+khulekani492@users.noreply.github.com>
2024-09-26 16:19:10 +0200

    Update elevator.py

--- elevator.py
+++ elevator.py
@@ -6,12 +6,11 @@
 
 
 
-
 # create a deck 
 
 from random import *
 import random
-#from random import Random
+from random import Random
 
 rng = random.Random()
 
@@ -19,40 +18,29 @@ Card = []
 numbers = [0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,]
 cards = {'Red ':numbers,'Yellow ':numbers,'blue ':numbers,'green ':numbers}
 
+# special_cards = []
+# for i in ['Red','RED','YELLOW','YELLOW','GREEN','GREEN','BLUE', 'BLUE']:
+#       c = "+2" + i
+#       special_cards.append(c)
+# for i in ['Red','RED','YELLOW','YELLOW','GREEN','GREEN','BLUE', 'BLUE']:
+#       b = "REV_" + i
+#       special_cards.append(b)
+# for i in ['Red','RED','YELLOW','YELLOW','GREEN','GREEN','BLUE', 'BLUE']:
+#     b = "SKIP^" + i
+#     special_cards.append(b)
+
+# for k,v in cards.items():
+#         for i in v:
+#             a = [k , str(i)]
+#             Card.append(a)
 
 
 
 
-
-special_cards = []
-for i in ['Red','RED','YELLOW','YELLOW','GREEN','GREEN','BLUE', 'BLUE']:
-      c = "+2" + i
-      special_cards.append(c)
-for i in ['Red','RED','YELLOW','YELLOW','GREEN','GREEN','BLUE', 'BLUE']:
-      b = "REV_" + i
-      special_cards.append(b)
-for i in ['Red','RED','YELLOW','YELLOW','GREEN','GREEN','BLUE', 'BLUE']:
-    b = "SKIP^" + i
-    special_cards.append(b)
-
-
-  
-   
-    #return n
-
-
-for k,v in cards.items():
-        for i in v:
-            a = [k , str(i)]
-            Card.append(a)
-
-
-
-
-# create a deck 
-Card = []
-numbers = [0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,]
-cards = {'RED':numbers,'YELLOW':numbers,'BLUE':numbers,'GREEN':numbers}
+ #create a deck 
+#Card = []
+# numbers = [0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,]
+# cards = {'RED':numbers,'YELLOW':numbers,'BLUE':numbers,'GREEN':numbers}
 
 special_cards = []
 for i in ['RED','RED','YELLOW','YELLOW','GREEN','GREEN','BLUE', 'BLUE']:
@@ -80,7 +68,7 @@ for k,v in cards.items():
             Card.append(a)
 Card.extend(special_cards) 
 
-
+print(len(Card))
 #shuffle_the_card 
 rng.shuffle(Card)
 
@@ -96,8 +84,8 @@ before_shuffle = Card
 
 Discarded_pile = [Card[0]]
     
-bottom_card = Discarded_pile[0]
 
+bottom_card = Discarded_pile[0]
 # Discarded assign bottom card
 del Card[0]
 #delete from the deck   
@@ -116,35 +104,45 @@ def bottom():
 
 def starting():
      
-     print('bottom..card',bottom())
+#      print('bottom..card',bottom())
+#      if bottom() == ['RED','+2'] or bottom() == ['GREEN','+2'] or bottom() == ['YELLOW','+2'] or bottom() == ['BLUE','+2']:
+#         Computer_hand.append(Card[:2])
+#         print("bottom ",bottom_card, " 2** cards added to computer")
+#      bot = bottom()
+#      print(bot)
 
      print("\nPlayerhand ", players_hand) 
-     take_or_play = input("\nPlease enter Play to play or 'take' to draw a card from the Deck: ")
+     take_or_play = input("\nPlease enter 'P' to play or 't' to'take' to draw a card from the Deck: ")
 
-     if take_or_play != 'play' and take_or_play != 'take':
+     if take_or_play.lower() != 'p' and take_or_play.lower() != 't':
             #print("ERROR\n Please enter 'play'  or 'take' to draw from the deck ")
             return starting()
      
            
      
-     if take_or_play.lower() == 'play':
-        user_input = int(input("\nPlease Enter a number(press '8' to restart): "))  
-        print(player_card(user_input)) 
-     elif take_or_play.lower() == 'take':
+     if take_or_play.lower() == 'p':
+        user_input = int(input("\nPlease Enter a number(press '8' to restart): \n"))  
+        print(player_card(user_input))
+        b = player_card(user_input) 
+        return ' new ', b
+     elif take_or_play.lower() == 't':
            players_hand.append(random.choice(Card)) 
-           print("\nCard have been added ", players_hand) 
-     
+           print("\nCard have been added \n", players_hand) 
+     print(Computer_card())
+      
+#      return player_card(user_input)
 
 def restart():
-     print("\n Player1 choosen a special card play again ...")
+     print("\n Player1 play again ...")
      starting()
 
 def invalid_card():
 
-      print("\nCard not match or you pressed a number above 8\n...Enter a valid card or take from the deck ")
+      print("\nInvalid entry\n...Enter a valid card or take from the deck ")
       starting()
 
 def player_card(user_input):
+     global bottom_card
      #global bottom_card 
      bottom_card = bottom()          
      for i in range(len(players_hand)):
@@ -154,85 +152,139 @@ def player_card(user_input):
                   if bottom_card[0] == choosen_card[0] or bottom_card[1] == choosen_card[1] or choosen_card == ['wilddraw','4'] or choosen_card == ['wild','4']:
                        
                        bottom_card = choosen_card #switching the indexed position so bottom card get updated
-                       
+                       del players_hand[i]     
+                 
                        if bottom_card == ['RED','REV_'] or bottom_card == ['BLUE','REV_'] or bottom_card == ['YELLOW','REV_'] or bottom_card == ['GREEN','REV_']:
                               del players_hand[i]
-                              print (players_hand)
+                  
                               return restart()
-                       if bottom_card == ['RED',"SKIP^" ] or bottom_card == ['BLUE','SKIP'] or bottom_card == ['YELLOW','SKIP^' ] or bottom_card == ['GREEN','SKIP^' ]:
+                       if bottom_card == ['RED',"SKIP^" ] or bottom_card == ['BLUE','SKIP^'] or bottom_card == ['YELLOW','SKIP^' ] or bottom_card == ['GREEN','SKIP^' ]:
                               del players_hand[i]
-                              print (players_hand)
+                              
                               return restart()
                        if bottom_card == ['RED','+2'] or bottom_card == ['GREEN','+2'] or bottom_card == ['YELLOW','+2'] or bottom_card == ['BLUE','+2']:
                              bottom_card = choosen_card
                              del players_hand[i]
-                             print (players_hand)
+                             
                         #check if there is no alternative card that exists in the player's hand
                              Computer_hand.append(Card[:2])
                              return "bottom ",bottom_card, " 2 cards added to computer"
                        if bottom_card == ["wild","4"]:
                              del players_hand[i]
                              bottom_card = choosen_card
-                             user =  input("You played a wild4 ,Choose a color:  ")
-                             for i in ["YELLOW","BLUE","GREEN","RED"]:
+                             user =  input("You played a wild4 ,Choose a color['YELLOW','BLUE','GREEN','RED']:  ")
+                             for i in  [ ["YELLOW"],["BLUE"],["GREEN","RED"]]:
                                   if i == user:
                                       bottom_card = i
                                       return "bottom card ", [bottom_card]
 
-                       del players_hand[i]      
+                        
                        return bottom_card
-                  
+                     
                  
                         
                   elif choosen_card == ['wildraw','4']:
                          for card in players_hand:
                                if bottom_card[0] == card[0] or bottom_card[1] == card[1]:
                                      players_hand.append(Card[:4])
-                                     return '4 ards added to play\n',players_hand
+                                     return '4 cards added to play\n',players_hand
                                      
 
-                         del players_hand[i]
+                         
                          bottom_card = choosen_card
+                         del players_hand[i]
                         #check if no alternative card exist in the play hand
                          Computer_hand.append(Card[:4])
                          return "bottom ",bottom_card, "\n4 cards added to computer" 
                   
-                  elif bottom_card != choosen_card:
-                        return invalid_card()
+                  # elif bottom_card[0] != choosen_card:
+                  #       return invalid_card()
               
            elif user_input > 8:
                  return invalid_card()
            elif user_input == 8:
-                 return restart()     
+                 return restart()
+      #      print(Computer_hand)
+      #      print(Computer_card())     
+
 
 
 def Computer_card():
     global bottom_card
     for i in range(len(Computer_hand)):
         choosen_card = Computer_hand[i]
+        
+        if bottom_card[0][0] == choosen_card[0][0] or bottom_card[0][1] == choosen_card[0][1] or choosen_card == ['wilddraw','4'] or choosen_card == ['wild','4']:
+                       
+                       bottom_card = choosen_card #switching the indexed position so bottom card get updated
+                       
+                  #      if bottom_card == ['RED','REV_'] or bottom_card == ['BLUE','REV_'] or bottom_card == ['YELLOW','REV_'] or bottom_card == ['GREEN','REV_']:
+                  #             del Computer_hand[i]
+                              
+                  #             return Computer_card()
+                  #      if bottom_card == ['RED',"SKIP^" ] or bottom_card == ['BLUE','SKIP^'] or bottom_card == ['YELLOW','SKIP^' ] or bottom_card == ['GREEN','SKIP^' ]:
+                  #             del Computer_hand[i]
+                              
+                  #             return Computer_card()
+                  #      if bottom_card == ['RED','+2'] or bottom_card == ['GREEN','+2'] or bottom_card == ['YELLOW','+2'] or bottom_card == ['BLUE','+2']:
+                  #            bottom_card = choosen_card
+                  #            del Computer_hand[i]
+                  #            players_hand.append(Card[:4])
+                  #            return "bottom ",bottom_card, "\n4 cards added to Player" 
+                       
+                             
+                  #      if bottom_card == ["wild","4"]:
+                  #            del Computer_hand[i]
+                  #            bottom_card = choosen_card
+                  #            #user =  input("You played a wild4 ,Choose a color['YELLOW','BLUE','GREEN','RED']:  ")
+                  #            colors = [ ["YELLOW"],["BLUE"],["GREEN","RED"]]
+                  #            com_chosen_color = random.choice(colors)
+                  #             #     if i == user:
+                  #            bottom_card = com_chosen_color
+                  #            return "bottom card ", [bottom_card]
+                  #      if bottom_card == ['wildraw','4']:
+                  #              del Computer_hand[i]
+                  #              bottom_card = choosen_card
+                  #       #check if no alternative card exist in the play hand
+                  #              players_hand.append(Card[:4])
+                  #              return "bottom ",bottom_card, "\n4 cards added to Player" 
+                       
 
-        if bottom_card[0] == choosen_card[0] or bottom_card[1] == choosen_card[1] or bottom_card[0] == ['wilddraw','4'] or bottom_card[0] == ['wild','4']:
-                   del Computer_hand[i]
-                   bottom_card = choosen_card #switching the indexed position so bottom card get updated
-                   return bottom_card
-                   
-        elif choosen_card == ['wild','4']:
-                    del players_hand[i]
-                    select_next_clr = ["Yellow","BLUE","GREEN","RED"]
-                    rand_choice = random.choice(select_next_clr)
-                    upper_case = rand_choice.upper()
-                    bottom_card = upper_case
-                    return bottom_card  
-        else:
-             Computer_hand.append(random.choice(Card))
-       
-             return "Computer_hand takes one card ", random.choice(Card)
+                       del Computer_hand[i]      
+                       return 'bottom 55 card ',bottom_card
+    else:
+      return "hhhh"
+        
+      #   elif bottom_card != choosen_card:
+      #        Computer_hand.append(Card[:2])
+      #        return 'COMPYTER took a card'
+#     else:
+#           return "ddddd"
+          
+        
+print(Computer_card())
+    
 
+# starting()
+index =  0
+while index < len(Card):
+ 
+ print('\nBottom card\n____________________________\n',bottom())
+ starting()
+ #print(bottom(), "after")
+ print('\nCOMPUTER TURN\n............................ ')
+ print('\n COMPUTER cards\n------',Computer_hand)
+ print(Computer_card())
 
-
-starting()
-
-
-
-
-
+ index += 1
+# #       bottom_card = bottom()
+#       print('bottom..card',bottom())
+#       if bottom() == ['RED','+2'] or bottom() == ['GREEN','+2'] or bottom() == ['YELLOW','+2'] or bottom() == ['BLUE','+2']:
+#         Computer_hand.append(Card[:2])
+#         print("bottom ",bottom_card, " 2** cards added to computer")
+#       starting()
+      
+      
+#       print("COmputer turn", Computer_hand)
+#       print(Computer_card())
+     
