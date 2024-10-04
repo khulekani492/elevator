18023973f99a3823d5ca141fbe590eeb7816cb6f
khulekani492 <77571282+khulekani492@users.noreply.github.com>
2024-10-04 16:09:04 +0200

    Update elevator.py

--- elevator.py
+++ elevator.py
@@ -20,3 +20,31 @@ while True:
     if change == 0:
         break
 
+def change(amount_paid,amount_due):
+       cash = {'R200':20000,'R100' : 10000,'R50': 5000, 'R20': 2000,'R10':1000,'R5':500,'R2':200, 'R1' :100 , '50c': 50,'20c':20,
+        '10c':10,'5c':5 }
+       change = amount_paid - amount_due
+
+       denominations = []
+       while True:
+            for k, v in cash.items():
+                if change >= v :
+                    change = change - v
+                    denominations.append(k)
+                    break
+            if change == 0:
+              break 
+    
+       final_change = set(denominations)
+
+       # converting denomationations list to a dictionary, keys being the denomations and coins 
+       # the values are the number of times that denomationtions or coin appears
+       #in the list append in the while loop
+       #final_change = set(denominations)
+
+       denominations_dict = {}
+       for denomination in final_change:
+            denominations_dict[denomination] = denominations.count(denomination)
+       return denominations_dict
+
+
