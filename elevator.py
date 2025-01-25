01ff5ba1848b4e4bc41a07886119478b5b6e9c2a
khulekani492 <77571282+khulekani492@users.noreply.github.com>
2025-01-25 22:43:41 +0200

    Update elevator.py

--- elevator.py
+++ elevator.py
@@ -40,3 +40,29 @@ def is_password_secure(password):
    
                 
     return True                    
+def grouping_into_bins (n):
+    a = [2,3,4,5,6,7,8,9,10,11,12]
+    divide_by_eleven = 11 / n
+    co = int(divide_by_eleven)
+    start = 0 
+    end = co
+    sub_list = [] 
+    for i in range(1,n+1):
+        print(a)
+        if i == n:
+            sub_list.append(a)
+        else:
+           bx = a[start:end]
+
+           sub_list.append(bx)
+           del a[:co] 
+    return sub_list 
+turn_into_STRINGS = grouping_into_bins(4)
+def histo_bins(turn):
+    histogram_bins = []
+    for sublist in turn:
+       take =  str(sublist[0]) + '-' + str(sublist[-1])
+       histogram_bins.append(take) 
+
+    return histogram_bins 
+print(histo_bins(turn_into_STRINGS))
